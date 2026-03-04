e0283d530604a1e5378c4d07412c790adf109436
Ashlee <ashleeramirezrosario@gmail.com>
2026-03-04 15:41:43 -0400

    Add project files.

---
+++ Calculo-de-depreciacion/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Calculo_de_depreciacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los datos:");
+            Console.WriteLine("Cantidad de años");
+            int tiempo = ValidarTiempo(int.Parse(Console.ReadLine()!));
+
+            Console.WriteLine("tasa de depreciacion anual");
+            decimal tasa = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine("costo original del producto");
+            decimal costoOriginal = decimal.Parse(Console.ReadLine()!);
+             
+            decimal valorActual  = SaldoDecreciente(tiempo, tasa, costoOriginal);
+            Console.WriteLine("Valor del activo en el ultimo año: " + Math.Round(valorActual,2));
+        }
+
+        static decimal SaldoDecreciente(int tiempo, decimal tasa, decimal costoOriginal)
+        {   
+            if (tiempo == 0)
+                return costoOriginal;
+
+            decimal factorRetencion = 1 - (tasa / 100);
+            return SaldoDecreciente(tiempo - 1,tasa, costoOriginal) * factorRetencion;
+        }
+
+        static int ValidarTiempo(int t)
+        {
+            if (t > 0)
+                return t;
+
+            Console.WriteLine("La cantidad de tiempo no puede ser negativa");
+            return ValidarTiempo(int.Parse(Console.ReadLine()!));
+        }
+    }
+}
---
+++ Interes-Compuesto/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Interes_Compuesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte los datos:");
+            Console.WriteLine("Cantidad de años transcurridos:");
+            int tiempo = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Capital inicial:");
+            decimal capitalInicial = decimal.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Tasa de interes anual:");
+            decimal TasaInteresAnual = decimal.Parse(Console.ReadLine()!);
+
+            decimal montoAcumulado = CalcularMontoTotalAcumulado(capitalInicial, TasaInteresAnual, tiempo);
+            Console.WriteLine("Monto total acumulado: " + Math.Round(montoAcumulado,2));
+        }
+
+        static decimal CalcularMontoTotalAcumulado(decimal m, decimal x, int n)
+        {
+            if (n == 0)
+                return m;
+
+            decimal interes = x / 100;
+
+            return CalcularMontoTotalAcumulado(m, x, n - 1) * (1 + interes);
+        }
+    }
+}
